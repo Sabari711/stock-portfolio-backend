c8abc31ffd5f10784f11e07c6792ca0cb786fca9
Sabari B <111430395+Sabari711@users.noreply.github.com>
2025-07-25 17:13:58 +0530

    Update db_connection.js

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,9 +1,6 @@
 const Mongoose = require('mongoose');
 const config = require("./config")
-   
-// const mongoDbUrl = {
-//     DB_HOST: `mongodb+srv://balasabari27:TkBtUjS5fonMPrIP@democluster.kj0hmur.mongodb.net/demo`
-// }
+
 const mongoDbUrl = {
     DB_HOST: `${config.HOST_URL}`
 }
